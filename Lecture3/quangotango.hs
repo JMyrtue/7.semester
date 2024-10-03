57c575156990f86cb350c338f8b254862b06ab54
Jonathan Myrtue Jensen <jojens15@student.aau.dk>
2024-10-03 21:30:20 +0200

    finishes third lecture

--- Lecture3/quangotango.hs
+++ Lecture3/quangotango.hs
@@ -1,8 +1,13 @@
 
 quango :: a -> [a]
 quango x = [x]
-tango (x, y) z = x + y + z
+
+tango (x, y) z = 5
 
 -- ChatGPT helped
-tango2 :: Num p1 => (a, b) -> p2 -> p1
-tango2 (x, y) z = fromIntegral (length (show x) + length (show y)) + 1
+-- tango2 (x, y) z = fromIntegral (length (show x) + length (show y)) + 1
+
+-- Ord x, Integral y => [x] -> y
+
+
+f x y (a, b) = if x == y then min a b else max a b
